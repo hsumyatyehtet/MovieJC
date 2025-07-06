a954d9f2324d0714d42c25ff3178a803a1037b34
YOMA\hsumyatyehtet <hsumyatyehtet@onenex.co>
2025-07-06 20:42:21 +0630

    navigation add

--- app/src/main/java/com/hmyh/moviejc/movieui/feature/detail/DetailMovie.kt
+++ app/src/main/java/com/hmyh/moviejc/movieui/feature/detail/DetailMovie.kt
@@ -1,11 +1,67 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.hmyh.moviejc.movieui.feature.detail
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
-fun DetailMovie(navController: NavController,movie: String?) {
+fun DetailMovie(navController: NavController,id: Long) {
 
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.Transparent
+                ),
+                title = {
 
+                },
+                navigationIcon = {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Back",
+                        modifier = Modifier.padding(8.dp).clickable {
+                            navController.popBackStack()
+                        },
+                        tint = MaterialTheme.colorScheme.onSurface
+                    )
+                }
+            )
+        }
+    ) {paddingValues->
+        Surface(
+            modifier = Modifier.fillMaxSize().padding(paddingValues)
+        ) {
+
+            MovieDetailContent(id)
+
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun MovieDetailContent(id: Long?=null) {
+
+    Text(text = id.toString())
 
 }
